e15e625c0ad696c5afb1fcd72c560a1f4c46e5a2
Junwei <mcpejunwei@gmail.com>
2026-06-21 00:31:03 -0400

    5 more exercises

---
+++ brocode/wordList.py
@@ -0,0 +1,17 @@
+#word for hangman game
+# Mammals
+mammals = ("lion", "tiger", "elephant", "kangaroo", "dolphin", "bear", "wolf")
+
+# Birds
+birds = ("eagle", "penguin", "hawk", "owl", "flamingo", "parrot", "swan")
+
+# Reptiles and Amphibians
+reptiles_amphibians = ("crocodile", "python", "frog", "toad", "chameleon", "turtle")
+
+# Aquatic Animals
+aquatic = ("shark", "whale", "octopus", "jellyfish", "starfish", "salmon")
+
+# Insects
+insects = ("butterfly", "ant", "bee", "beetle", "dragonfly", "mantis")
+
+words = mammals + birds + reptiles_amphibians + aquatic + insects
